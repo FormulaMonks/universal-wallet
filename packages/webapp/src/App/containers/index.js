a11f30721e3018347098c22cb38ed7cc2a0b8bcc
Gorka <gorka@aquigorka.com>
2018-05-16 09:34:42 +0200

    Containers: remove login and logout mocks

--- packages/webapp/src/App/containers/index.js
+++ packages/webapp/src/App/containers/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Header } from '../components'
+import { Header } from '../components';
 
-const Mock = ({ section }) => <div>
-  <Header />
-  {section}
-</div>;
+const Mock = ({ section }) => (
+  <div>
+    <Header />
+    {section}
+  </div>
+);
 
-export const Login = () => <Mock section="login" />;
 export const Balance = () => <Mock section="balance + receive" />;
 export const Send = () => <Mock section="send" />;
 export const Exchange = () => <Mock section="in wallet exchange" />;
@@ -14,4 +15,3 @@ export const Wallets = () => <Mock section="wallets" />;
 export const Transactions = () => <Mock section="transaction history" />;
 export const AddressBook = () => <Mock section="address book" />;
 export const NotFound = () => <Mock section="404" />;
-export const Logout = () => <Mock section="logout" />;
