36db2d6206870bb2bd2ec1939488ef6dbb4c3540
Gorka <gorka@aquigorka.com>
2018-05-17 12:43:32 +0200

    Update app to new file structure

--- packages/webapp/src/App/index.js
+++ packages/webapp/src/App/index.js
@@ -5,34 +5,40 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
-import {
-  Balance,
-  Send,
-  Exchange,
-  Wallets,
-  Transactions,
-  AddressBook,
-  NotFound,
-} from './containers';
-import { Auth } from './components';
+import Balance from './Balance';
+import Send from './Send';
+import Wallets from './Wallets';
+import Contacts from './Contacts';
+import { Auth } from '../components';
+
+import { Header } from '../components';
+const Mock = ({ section }) => (
+  <div>
+    <Header />
+    {section}
+  </div>
+);
+const Exchange = () => <Mock section="in wallet exchange" />;
+const Transactions = () => <Mock section="transaction history" />;
+const NotFound = () => <Mock section="404" />;
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route path="/404" component={NotFound} />
-          <Redirect exact from="/" to="/balance" />
-          <Auth>
+        <Auth>
+          <Switch>
+            <Redirect exact from="/" to="/balance" />
+            <Route path="/404" component={NotFound} />
             <Route path="/balance" component={Balance} />
             <Route path="/send" component={Send} />
             <Route path="/in-wallet-exchange" component={Exchange} />
             <Route path="/wallets" component={Wallets} />
             <Route path="/transactions" component={Transactions} />
-            <Route path="/address-book" component={AddressBook} />
-          </Auth>
-          <Redirect from="*" to="/404" />
-        </Switch>
+            <Route path="/contacts" component={Contacts} />
+            <Redirect from="*" to="/404" />
+          </Switch>
+        </Auth>
       </Router>
     );
   }
