8ec1566065c01a375816c63c62a3dd1102e91ee0
Gorka <gorka@aquigorka.com>
2018-05-17 12:44:02 +0200

    Stores: add wallets store

---
+++ packages/webapp/src/stores/Wallets.js
@@ -0,0 +1,83 @@
+import React, { Component, Fragment, Children, cloneElement } from 'react';
+import uuid from 'uuid';
+import { getFile, putFile } from 'blockstack';
+import { WALLETS_JSON } from '../utils/constants';
+
+export default class Wallets extends Component {
+  state = {
+    error: null,
+    loading: true,
+    wallets: [],
+  };
+
+  componentDidMount() {
+    this.get();
+  }
+
+  render() {
+    const { wallets, loading, error } = this.state;
+    const { children, ...rest } = this.props;
+    return (
+      <Fragment>
+        {Children.map(this.props.children, child =>
+          cloneElement(child, {
+            wallets,
+            walletsError: error && (
+              <div>There was an error fetching the wallets: {error}</div>
+            ),
+            walletsLoading: loading,
+            walletsGet: this.get,
+            walletsDelete: this.delete,
+            walletsPut: this.put,
+            walletsPost: this.post,
+            ...rest,
+          }),
+        )}
+      </Fragment>
+    );
+  }
+
+  delete = async walletId => {
+    this.setState({ loading: true })
+
+    const newList = this.state.wallets.filter(({ id }) => id !== walletId);
+    await putFile(WALLETS_JSON, JSON.stringify(newList));
+  };
+
+  get = async () => {
+    this.setState({ loading: true });
+
+    try {
+      const file = await getFile(WALLETS_JSON);
+      const wallets = JSON.parse(file || '[]');
+      this.setState({ wallets, loading: false });
+    } catch (e) {
+      this.setState({ error: e.toString() });
+    }
+  };
+
+  post = async obj => {
+    this.setState({ loading: true });
+
+    const newObj = {
+      ...obj,
+      id: uuid(),
+      createdAt: Date.now(),
+      lastModified: Date.now(),
+    };
+    const newList = [...this.state.wallets, newObj];
+    await putFile(WALLETS_JSON, JSON.stringify(newList));
+  };
+
+  put = async (walletId, obj) => {
+    this.setState({ loading: true });
+
+    const { wallets } = this.state;
+    const { id, createdAt, lastModified, ...current } = wallets.find(({ id }) => id === walletId);
+    const newList = [
+      ...wallets.filter(w => w.id !== id),
+      { ...current, ...obj, id, createdAt, lastModified: Date.now() },
+    ];
+    await putFile(WALLETS_JSON, JSON.stringify(newList));
+  };
+}
