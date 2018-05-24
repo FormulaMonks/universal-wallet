42ca245554f747f4d2b9f0229240c48e10e2cfc7
Gorka <gorka@aquigorka.com>
2018-05-24 14:29:37 +0200

    Utils: add code format

--- packages/webapp/src/utils/constants.js
+++ packages/webapp/src/utils/constants.js
@@ -5,10 +5,16 @@ export const CONTACTS_JSON = 'contacts.json';
 export const BTC_TO_USD =
   'https://www.bitstamp.net/api/v2/ticker/btcusd/?cors=1';
 
-export const SHAPESHIFT = 'https://shapeshift.io/'
+export const SHAPESHIFT = 'https://shapeshift.io/';
 
-export const SHAPESHIFT_MARKETINFO = `${SHAPESHIFT}marketinfo/`;
+export const SHAPESHIFT_MARKET_INFO = `${SHAPESHIFT}marketinfo/`;
 
 export const SHAPESHIFT_GETCOINS = `${SHAPESHIFT}getcoins/`;
 
-export const BITCOIN_SYMBOL_LOWER_CASED = 'btc'
+export const SHAPESHIFT_SHIFT = `${SHAPESHIFT}shift/`;
+
+export const SHAPESHIFT_SEND_AMOUNT = `${SHAPESHIFT}sendamount/`;
+
+export const SHAPESHIFT_VALIDATE_ADDRESS = `${SHAPESHIFT}validateAddress/`;
+
+export const BITCOIN_SYMBOL_LOWER_CASED = 'btc';
