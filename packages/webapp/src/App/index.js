90da1670172c81f2932da93781d9bba6dbd75804
Gorka <gorka@aquigorka.com>
2018-05-29 12:17:42 +0200

    App: remove old sections and add home

--- packages/webapp/src/App/index.js
+++ packages/webapp/src/App/index.js
@@ -5,12 +5,9 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
-import Balance from './Balance';
-import Send from './Send';
+import Home from './Home'
 import Wallets from './Wallets';
 import Contacts from './Contacts';
-import Transactions from './Transactions';
-import Exchange from './Exchange'
 import { Auth } from '../components';
 
 const NotFound = () => <div>404</div>;
@@ -21,13 +18,9 @@ class App extends Component {
       <Router>
         <Auth>
           <Switch>
-            <Redirect exact from="/" to="/balance" />
             <Route path="/404" component={NotFound} />
-            <Route path="/balance" component={Balance} />
-            <Route path="/send" component={Send} />
-            <Route path="/in-wallet-exchange" component={Exchange} />
+            <Route exact path="/" component={Home} />
             <Route path="/wallets" component={Wallets} />
-            <Route path="/transactions" component={Transactions} />
             <Route path="/contacts" component={Contacts} />
             <Redirect from="*" to="/404" />
           </Switch>
