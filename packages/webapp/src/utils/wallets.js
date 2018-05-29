2fd7ff084c4c8552ad63db343a28d1a7a5b019b2
Gorka <gorka@aquigorka.com>
2018-05-29 15:45:25 +0200

    Wallets utils: add generate eth wallet

--- packages/webapp/src/utils/wallets.js
+++ packages/webapp/src/utils/wallets.js
@@ -1,7 +1,26 @@
 import bitcore from 'bitcore-lib';
-import { BITCOIN_SYMBOL_LOWER_CASED } from './constants';
+import {
+  BITCOIN_SYMBOL_LOWER_CASED,
+  ETHER_SYMBOL_LOWER_CASED,
+} from './constants';
+import { createAccount } from './ethTx';
 
-export const generateNewWallet = symbol => {
+export const AVAILABLE_WALLET_GENERATION_COINS = [
+  { name: 'Bitcoin ', symbol: BITCOIN_SYMBOL_LOWER_CASED.toUpperCase() },
+  { name: 'Ethereum', symbol: ETHER_SYMBOL_LOWER_CASED.toUpperCase() },
+];
+
+const generateNewEthWallet = () => {
+  const { address: publicAddress, privateKey } = createAccount();
+
+  return {
+    privateKey,
+    publicAddress,
+    symbol: ETHER_SYMBOL_LOWER_CASED.toUpperCase(),
+  };
+};
+
+const generateNewBtcWallet = () => {
   const randBuf = bitcore.crypto.Random.getRandomBuffer(32);
   const randNum = bitcore.crypto.BN.fromBuffer(randBuf);
   const privateKey = new bitcore.PrivateKey(randNum);
@@ -12,4 +31,18 @@ export const generateNewWallet = symbol => {
     publicAddress,
     symbol: BITCOIN_SYMBOL_LOWER_CASED.toUpperCase(),
   };
+};
+
+export const generateNewWallet = symbol => {
+  if (
+    !AVAILABLE_WALLET_GENERATION_COINS.find(
+      coin => coin.symbol.toLowerCase() === symbol.toLowerCase(),
+    )
+  ) {
+    throw new Error(`Cannot generate wallet for ${symbol}`);
+  }
+
+  return symbol.toLowerCase() === ETHER_SYMBOL_LOWER_CASED
+    ? generateNewEthWallet()
+    : generateNewBtcWallet();
 };
