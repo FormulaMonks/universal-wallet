29b79096bf43368469a4af268cf5215f7206f1d7
Gorka <gorka@aquigorka.com>
2018-05-29 12:12:07 +0200

    Contacts: refactor into store + saga

--- packages/webapp/src/components/Contacts.js
+++ packages/webapp/src/components/Contacts.js
@@ -2,25 +2,27 @@ import React, { Component, Fragment, Children, cloneElement } from 'react';
 import uuid from 'uuid';
 import { getFile, putFile } from 'blockstack';
 import { CONTACTS_JSON } from '../utils/constants';
+import composeStore from '../utils/composeStore';
 
-export default class contacts extends Component {
+export const sort = (a, b) => a.alias.localeCompare(b.alias);
+
+class Store extends Component {
   state = {
     error: null,
     loading: true,
     contacts: [],
+    contact: null,
   };
 
-  componentDidMount() {
-    this.get();
-  }
-
   render() {
-    const { contacts, loading, error } = this.state;
+    const { contact, contacts, loading, error } = this.state;
     const { children, ...rest } = this.props;
     return (
       <Fragment>
         {Children.map(this.props.children, child =>
           cloneElement(child, {
+            ...rest,
+            contact,
             contacts,
             contactsError: error && (
               <div>There was an error fetching the contacts: {error}</div>
@@ -30,7 +32,8 @@ export default class contacts extends Component {
             contactsDelete: this.delete,
             contactsPut: this.put,
             contactsPost: this.post,
-            ...rest,
+            contactPick: this.pick,
+            contactRelease: this.release,
           }),
         )}
       </Fragment>
@@ -49,11 +52,20 @@ export default class contacts extends Component {
 
     try {
       const file = await getFile(CONTACTS_JSON);
-      const contacts = JSON.parse(file || '[]');
+      const rawContacts = JSON.parse(file || '[]');
+      const contacts = rawContacts.sort(sort);
       this.setState({ contacts, loading: false });
     } catch (e) {
       this.setState({ error: e.toString() });
     }
+  };
+
+  pick = contactId => {
+    const contact = this.state.contacts.find(({ id }) => contactId === id);
+    if (!contact) {
+      return;
+    }
+    this.setState({ contact });
   };
 
   post = async obj => {
@@ -67,6 +79,7 @@ export default class contacts extends Component {
     };
     const newList = [...this.state.contacts, newObj];
     await putFile(CONTACTS_JSON, JSON.stringify(newList));
+    return newObj;
   };
 
   put = async (contactId, obj) => {
@@ -82,4 +95,69 @@ export default class contacts extends Component {
     ];
     await putFile(CONTACTS_JSON, JSON.stringify(newList));
   };
+
+  release = () => {
+    this.setState({ contact: null });
+  };
 }
+
+class Saga extends Component {
+  componentDidMount() {
+    this.props.contactsGet();
+  }
+
+  render() {
+    const {
+      children,
+      contactsDelete,
+      contactsPut,
+      contactsPost,
+      ...rest
+    } = this.props;
+    return (
+      <Fragment>
+        {Children.map(children, child =>
+          cloneElement(child, {
+            ...rest,
+            contactsDelete: this.delete,
+            contactsPut: this.put,
+            contactsPost: this.post,
+          }),
+        )}
+      </Fragment>
+    );
+  }
+
+  delete = async contactId => {
+    const { contact, contactRelease, contactsDelete, contactsGet } = this.props;
+    if (contact && contact.id === contactId) {
+      contactRelease();
+    }
+    await contactsDelete(contactId);
+    await contactsGet();
+  };
+
+  put = async (contactId, data) => {
+    const {
+      contactRelease,
+      contactsPut,
+      contactPick,
+      contactsGet,
+    } = this.props;
+    contactRelease();
+    await contactsPut(contactId, data);
+    await contactsGet();
+    contactPick(contactId);
+  };
+
+  post = async data => {
+    const { contactsPost, contactsGet, contactPick } = this.props;
+    const newContact = await contactsPost(data);
+    await contactsGet();
+    contactPick(newContact.id);
+  };
+}
+
+const store = composeStore(Store, Saga);
+
+export { store as Store };
