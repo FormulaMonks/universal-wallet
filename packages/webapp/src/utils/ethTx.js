f6a656e2993467c183ec1a6ea9b596391b03ff5f
Gorka <gorka@aquigorka.com>
2018-05-30 14:34:49 +0200

    Eth utils: use fixed gas limit per tx and fix private key usage from eth generator

--- packages/webapp/src/utils/ethTx.js
+++ packages/webapp/src/utils/ethTx.js
@@ -2,46 +2,40 @@ import Web3 from 'web3';
 import EthereumTx from 'ethereumjs-tx';
 import { INFURA_NETWORK, INFURA_CHAIN_ID } from './constants';
 
+const GAS_LIMIT_IN_WEI = 21000
 const { eth, utils } = new Web3(
   new Web3.providers.HttpProvider(INFURA_NETWORK),
 );
 const chainId = INFURA_CHAIN_ID;
 const sendTx = async tx =>
-  new Promise(r =>
+  new Promise((resolve, reject) =>
     eth.sendSignedTransaction(`0x${tx.toString('hex')}`, (err, res) => {
       if (err) {
-        throw err;
+        reject(err);
       }
-      r(res);
+      resolve(res);
     }),
   );
 
 export const createAccount = () => eth.accounts.create();
 
 export const getTxInfo = async () => {
-  const { gasLimit: gasLimitInWei, gasUsed: gasUsedInWei } = await eth.getBlock(
-    'latest',
-  );
   const priceInWei = await eth.getGasPrice();
-  const maxFeeInWei = gasLimitInWei * priceInWei;
-  const aproxFeeInWei = gasUsedInWei * priceInWei;
+  const aproxFeeInWei = GAS_LIMIT_IN_WEI * priceInWei;
   return {
     wei: {
       price: priceInWei,
-      limit: gasLimitInWei,
-      maxFee: maxFeeInWei,
+      limit: GAS_LIMIT_IN_WEI,
       aproxFee: aproxFeeInWei,
     },
     ether: {
-      limit: utils.fromWei(`${gasLimitInWei}`, 'ether'),
+      limit: utils.fromWei(`${GAS_LIMIT_IN_WEI}`, 'ether'),
       price: utils.fromWei(`${priceInWei}`, 'ether'),
-      maxFee: utils.fromWei(`${maxFeeInWei}`, 'ether'),
       aproxFee: utils.fromWei(`${aproxFeeInWei}`, 'ether'),
     },
     gwei: {
-      limit: utils.fromWei(`${gasLimitInWei}`, 'gwei'),
+      limit: utils.fromWei(`${GAS_LIMIT_IN_WEI}`, 'gwei'),
       price: utils.fromWei(`${priceInWei}`, 'gwei'),
-      maxFee: utils.fromWei(`${maxFeeInWei}`, 'gwei'),
       aproxFee: utils.fromWei(`${aproxFeeInWei}`, 'gwei'),
     },
   };
@@ -49,18 +43,18 @@ export const getTxInfo = async () => {
 
 export const validateAddress = utils.isAddress;
 
-export const generateTx = async ({ to, from, privateKey, amount }) => {
+export const generateTx = async ({ to, from, privateKey: rawPK, amount }) => {
+  const privateKey = rawPK.indexOf('0x') === 0 ? rawPK.substr(2) : rawPK;
   const nonce = await eth.getTransactionCount(from);
   const valueInWei = utils.toWei(`${amount}`, 'ether');
   const gasPriceInWei = await eth.getGasPrice();
-  const { gasLimit: gasLimitInWei } = await eth.getBlock('latest');
   const tx = new EthereumTx({
     to,
     from,
     nonce: utils.toHex(nonce),
     value: utils.toHex(valueInWei),
     gasPrice: utils.toHex(gasPriceInWei),
-    gasLimit: utils.toHex(gasLimitInWei),
+    gasLimit: utils.toHex(GAS_LIMIT_IN_WEI),
     chainId,
   });
   tx.sign(Buffer.from(privateKey, 'hex'));
