f7e678c3f315caf82435f805436f54a28e89762b
Gorka <gorka@aquigorka.com>
2018-05-30 14:52:10 +0200

    Header: add testnet notification

--- packages/webapp/src/components/Header.js
+++ packages/webapp/src/components/Header.js
@@ -1,18 +1,31 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import { signUserOut } from 'blockstack'
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { signUserOut } from 'blockstack';
 
 export default class Header extends Component {
   render() {
-    return <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="wallets">Wallets</Link></li>
-      <li><Link to="contacts">Contacts</Link></li>
-      <li><button onClick={this.onLogout}>Logout</button></li>
-    </ul>
+    return (
+      <Fragment>
+        {process.env.REACT_APP_TESTNET ? <div>Testnet ON</div> : null}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="wallets">Wallets</Link>
+          </li>
+          <li>
+            <Link to="contacts">Contacts</Link>
+          </li>
+          <li>
+            <button onClick={this.onLogout}>Logout</button>
+          </li>
+        </ul>
+      </Fragment>
+    );
   }
 
   onLogout = () => {
-    signUserOut(window.location.origin)
-  }
+    signUserOut(window.location.origin);
+  };
 }
