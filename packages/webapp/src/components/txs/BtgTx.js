6c9dfc814531f5e96c32d0d6ef3fe742c7355647
Gorka <gorka@aquigorka.com>
2018-06-04 16:50:21 +0200

    Add btg utils and tx

---
+++ packages/webapp/src/components/txs/BtgTx.js
@@ -0,0 +1,125 @@
+import React, { Component, Fragment, Children, cloneElement } from 'react';
+import {
+  BITCOIN_GOLD_SYMBOL_LOWER_CASED,
+  broadcast,
+  fetchFee,
+  validateAddress,
+} from '../../utils/btg';
+import { propsChanged, validProps } from '../../utils/tx';
+
+const INITIAL_STATE = {
+  broadcasting: false,
+  checking: false,
+  error: null,
+  info: null,
+  txId: null,
+  valid: false,
+};
+
+const validSymbols = ({ toSymbol, fromSymbol }) =>
+  toSymbol.toLowerCase() === BITCOIN_GOLD_SYMBOL_LOWER_CASED &&
+  fromSymbol.toLowerCase() === BITCOIN_GOLD_SYMBOL_LOWER_CASED;
+
+const txValidProps = props => validProps(props) && validSymbols(props);
+
+export default class BtgTx extends Component {
+  state = { ...INITIAL_STATE };
+
+  componentDidMount() {
+    txValidProps(this.props) && this.validate();
+  }
+
+  componentDidUpdate(prevProps) {
+    propsChanged(this.props, prevProps) &&
+      txValidProps(this.props) &&
+      this.validate();
+  }
+
+  render() {
+    const { valid, error, checking, info, broadcasting, txId } = this.state;
+    const { children, ...rest } = this.props;
+    const { txError, txValid, txChecking } = rest;
+
+    if (!txValid || txError || txChecking || !txValidProps(this.props)) {
+      return (
+        <Fragment>
+          {Children.map(children, child => cloneElement(child, { ...rest }))}
+        </Fragment>
+      );
+    }
+
+    return (
+      <Fragment>
+        {Children.map(children, child =>
+          cloneElement(child, {
+            ...rest,
+            txBroadcast: this.broadcast,
+            txBroadcasting: broadcasting,
+            txChecking: checking,
+            txError: error,
+            txInfo: info,
+            txId: txId,
+            txValid: valid,
+          }),
+        )}
+      </Fragment>
+    );
+  }
+
+  broadcast = async () => {
+    this.setState({ broadcasting: <div>Broadcasting transaction</div> });
+    try {
+      const txId = await broadcast(this.props);
+      this.setState({ txId, broadcasting: <div>Transaction broadcasted</div> });
+    } catch (e) {
+      this.setState({
+        error: <div>{e.toString ? e.toString() : JSON.stringify(e)}</div>,
+        broadcasting: <div>The transaction was not broadcasted</div>,
+      });
+    }
+  };
+
+  validAmountFeeBalance(amount, fee, balance) {
+    this.setState({
+      checking: <div>{'Validating Amount + Fee < Balance'}</div>,
+    });
+    if (amount + fee > balance) {
+      this.setState({
+        error: <div>Cannot send amount bigger than balance + fee</div>,
+      });
+      return false;
+    }
+    return true;
+  }
+
+  validAddresses(to, from) {
+    this.setState({ checking: <div>Validating To address</div> });
+    if (!validateAddress(to)) {
+      this.setState({ error: <div>To is not a valid bitcoin gold address</div> });
+      return false;
+    }
+    this.setState({ checking: <div>Validating From address</div> });
+    if (!validateAddress(from)) {
+      this.setState({ error: <div>From is not a valid bitcoin gold address</div> });
+      return false;
+    }
+    return true;
+  }
+
+  validate = async () => {
+    this.setState({ ...INITIAL_STATE, checking: <div>Performing checks</div> });
+    const { to, from, amount, balance, privateKey } = this.props;
+    if (this.validAddresses(to, from)) {
+      try {
+        const fee = await fetchFee({ to, from, privateKey, amount });
+        const valid = this.validAmountFeeBalance(amount, fee, balance);
+        this.setState({ info: <div>Fee: {fee}</div>, valid });
+      } catch (e) {
+        this.setState({
+          error: <div>{e.toString ? e.toString() : JSON.stringify(e)}</div>,
+        });
+      }
+    }
+    this.setState({ checking: null });
+  };
+}
---
+++ packages/webapp/src/utils/btg.js
@@ -0,0 +1,108 @@
+import { Insight } from 'bitcore-explorers';
+import btgjs from 'bgoldjs-lib';
+import Wif from 'wif';
+
+const { Transaction, Address } = btgjs;
+const NETWORK = process.env.REACT_APP_TESTNET
+  ? btgjs.networks.testnet
+  : btgjs.networks.bitcoingold;
+const insight = process.env.REACT_APP_TESTNET
+  ? new Insight('https://test-explorer.bitcoingold.org/insight-api/')
+  : new Insight('https://explorer.bitcoingold.org/insight-api/');
+
+const toSatoshi = btc => btc * 100000000;
+const toBTC = satoshi => satoshi / 100000000;
+
+const getUnspentUtxos = address =>
+  new Promise((resolve, reject) =>
+    insight.getUnspentUtxos(address, (err, utxos) => {
+      if (err) {
+        reject(`Could not get unspent utxos: ${err}`);
+      }
+      resolve(utxos);
+    }),
+  );
+const generateTx = ({ utxos, fromAddress, toAddress, privateKey, amount }) => {
+  const mappedUtxos = utxos.map(
+    ({ address, txid, vout, scriptPubKey, satoshis }) => ({
+      txid,
+      vout,
+      scriptPubKey,
+      satoshis,
+    }),
+  );
+  const tx = Transaction();
+  tx.from(mappedUtxos);
+  tx.to(toAddress, toSatoshi(amount));
+  tx.change(fromAddress);
+  try {
+    tx.sign(privateKey);
+    tx.serialize();
+  } catch (err) {
+    throw new Error(`Could not sign & serialize transaction: ${err}`);
+  }
+  return tx;
+};
+
+const broadcastTx = tx =>
+  new Promise((resolve, reject) =>
+    insight.broadcast(tx.toString(), (err, txId) => {
+      if (err) {
+        reject(`Could not broadcast transaction: ${err}`);
+      }
+      resolve(txId);
+    }),
+  );
+
+export const BITCOIN_GOLD_SYMBOL_LOWER_CASED = 'btg';
+
+export const generateBtgWallet = () => {
+  const keyPair = btgjs.ECPair.makeRandom({
+    network: NETWORK,
+  });
+  const publicAddress = keyPair.getAddress().toString();
+  const privateKey = Wif.decode(keyPair.toWIF(NETWORK)).privateKey.toString(
+    'hex',
+  );
+
+  return {
+    privateKey,
+    publicAddress,
+    symbol: BITCOIN_GOLD_SYMBOL_LOWER_CASED,
+  };
+};
+
+export const validateAddress = address => Address.isValid(address, NETWORK);
+
+export const fetchFee = async ({ to, from, privateKey, amount }) => {
+  const fromAddress = Address.fromString(from, NETWORK);
+  const toAddress = Address.fromString(to, NETWORK);
+  const utxos = await getUnspentUtxos(fromAddress);
+  const tx = generateTx({
+    utxos,
+    fromAddress,
+    toAddress,
+    privateKey,
+    amount,
+  });
+  const { inputs, outputs } = tx.toObject();
+  const totalInputs = inputs.reduce((p, c) => p + c.output.satoshis, 0);
+  const totalOutputs = outputs.reduce((p, c) => p + c.satoshis, 0);
+  const fee = totalInputs - totalOutputs;
+  return toBTC(fee);
+};
+
+export const broadcast = async ({ to, from, privateKey, amount }) => {
+  const fromAddress = Address.fromString(from, NETWORK);
+  const toAddress = Address.fromString(to, NETWORK);
+  const utxos = await getUnspentUtxos(fromAddress);
+  const tx = generateTx({
+    utxos,
+    fromAddress,
+    toAddress,
+    privateKey,
+    amount,
+  });
+  const { result } = await broadcastTx(tx);
+  return result;
+};
