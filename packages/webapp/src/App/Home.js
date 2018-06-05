cdcda30aff770aa1ea5f9479f9e65d55fb4b5284
Gorka <gorka@aquigorka.com>
2018-06-05 16:18:08 +0200

    Home: update tree structure to remove unnecesary composition from home

--- packages/webapp/src/App/Home.js
+++ packages/webapp/src/App/Home.js
@@ -1,42 +1,14 @@
 import React, { Fragment } from 'react';
-import {
-  CoinsStore,
-  ContactsStore,
-  NavHeader,
-  TransactionsStore,
-  TransactionsView,
-  TxStore,
-  TxSetup,
-  WalletView,
-  WalletsStore,
-  WalletsView,
-} from '../components';
+import { CoinsStore, NavHeader, Wallets } from '../components';
 import { Section } from '../theme';
-
-const Home = props => (
-  <Fragment>
-    <Section>
-      <WalletsView {...props} />
-    </Section>
-    <WalletView {...props} />
-    <TxSetup {...props} />
-    <TransactionsView {...props} />
-  </Fragment>
-);
 
 export default () => (
   <Fragment>
     <NavHeader />
-    <WalletsStore>
-      <TransactionsStore>
-        <CoinsStore>
-          <ContactsStore>
-            <TxStore>
-              <Home />
-            </TxStore>
-          </ContactsStore>
-        </CoinsStore>
-      </TransactionsStore>
-    </WalletsStore>
+    <Section>
+      <CoinsStore>
+        <Wallets />
+      </CoinsStore>
+    </Section>
   </Fragment>
 );
