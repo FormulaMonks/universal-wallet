d3d604513eec1724e9dff2913cf7f42fdfe90854
Gorka <gorka@aquigorka.com>
2018-06-06 17:34:32 +0200

    Tx: add hifi designs

--- packages/webapp/src/components/Tx.js
+++ packages/webapp/src/components/Tx.js
@@ -7,8 +7,36 @@ import {
   BtgTxStore,
   EthTxStore,
 } from './txs';
+import Compose from './Compose';
+import styled from 'styled-components';
+import { Leaders, Dots, Button } from '../theme';
 
-export const View = ({
+const WrapTx = styled.div`
+  background: rgba(200, 200, 200, 0.1);
+  padding: 1em;
+`;
+
+const LeadersError = Leaders.extend`
+  font-weight: bold;
+  color: #b31313;
+`;
+
+const Title = styled.h5`
+  margin: 0.5em 0 2em 0;
+`;
+
+const H5 = styled.h5`
+  padding: 0;
+  margin: 0;
+  display: block;
+`;
+
+const DivTx = styled.div`
+  overflow: auto;
+  max-width: 250px;
+`;
+
+const View = ({
   txId,
   txBroadcast,
   txBroadcasting,
@@ -16,25 +44,81 @@ export const View = ({
   txError,
   txChecking,
   txInfo,
-}) => (
-  <Fragment>
-    {txError}
-    {txChecking}
-    {txInfo}
-    {txBroadcasting}
-    {txValid &&
-      !txBroadcasting &&
-      !txId && (
-        <Fragment>
-          <div>Tx can take place</div>
-          <button onClick={txBroadcast}>Send Tx</button>
-        </Fragment>
-      )}
-    {txId && <div>Transaction id: {txId}</div>}
-  </Fragment>
-);
+}) => {
+  return (
+    <WrapTx>
+      <Title>Transaction Details</Title>
 
-export const Store = ({ children, ...props }) => (
+      {!(
+        txId ||
+        txBroadcast ||
+        txBroadcasting ||
+        txValid ||
+        txError ||
+        txChecking ||
+        txInfo
+      ) && (
+        <Leaders>
+          Required fields
+          <Dots />
+          Amount and deposit address
+        </Leaders>
+      )}
+
+      {!!txError && (
+        <LeadersError>
+          <div>Error</div>
+          <Dots />
+          {txError}
+        </LeadersError>
+      )}
+
+      {!!txChecking && (
+        <Leaders>
+          <div>Status</div>
+          <Dots />
+          {txChecking}
+        </Leaders>
+      )}
+
+      {!!txInfo &&
+        !!txInfo.length &&
+        txInfo.map(({ label, value }, index) => (
+          <Leaders key={`tx-info-${index}`}>
+            {label}
+            <Dots />
+            {value}
+          </Leaders>
+        ))}
+
+      {!!txBroadcasting && (
+        <Leaders>
+          <div>Broadcast</div>
+          <Dots />
+          {txBroadcasting}
+        </Leaders>
+      )}
+
+      {!!txValid &&
+        !txBroadcasting &&
+        !txId && (
+          <Fragment>
+            <Button onClick={txBroadcast}>Send</Button>
+          </Fragment>
+        )}
+
+      {!!txId && (
+        <Leaders>
+          <H5>TxId</H5>
+          <Dots />
+          <DivTx>{txId}</DivTx>
+        </Leaders>
+      )}
+    </WrapTx>
+  );
+};
+
+const Store = ({ children, ...props }) => (
   <TxStore {...props}>
     <SsTxStore>
       <BtcTxStore>
@@ -47,3 +131,6 @@ export const Store = ({ children, ...props }) => (
     </SsTxStore>
   </TxStore>
 );
+
+export { View, Store };
+export default Compose(Store, View);
