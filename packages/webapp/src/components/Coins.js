1eecf4e0e83486a6d9c47db98a89fe66d4e9fc64
Gorka <gorka@aquigorka.com>
2018-06-06 17:25:54 +0200

    Coins: add default view to use hidden select and icon display

--- packages/webapp/src/components/Coins.js
+++ packages/webapp/src/components/Coins.js
@@ -1,6 +1,31 @@
 import React, { Component, Fragment, Children, cloneElement } from 'react';
-import { SHAPESHIFT_GETCOINS } from '../utils/ss';
+import { SHAPESHIFT, SHAPESHIFT_GETCOINS } from '../utils/ss';
 import Compose from '../components/Compose';
+import styled from 'styled-components';
+
+const DivSelect = styled.div`
+  position: relative;
+`;
+
+const DivI = styled.div`
+  font-size: 20px;
+  color: #444;
+  display: ${({ symbol }) => (symbol === '' ? 'block' : 'none')};
+`;
+
+const Select = styled.select`
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  right: 0;
+  top: 0;
+  opacity: 0;
+  cursor: pointer;
+`;
+
+const Image = ({ imageSmall, symbol }) => (
+  <img src={`${SHAPESHIFT}${imageSmall}`} alt={symbol} />
+);
 
 export const View = ({
   coin,
@@ -13,23 +38,34 @@ export const View = ({
 }) => {
   const onChangeHandler = onChange || (e => coinPick(e.currentTarget.value));
   const controlledValue = value || (coin && coin.symbol) || '';
+
   return (
     <Fragment>
       {coinsError}
-      {coinsLoading ? (
-        <div>loading</div>
-      ) : (
-        <select value={controlledValue} onChange={onChangeHandler}>
-          <option disabled value="" hidden>
-            Coins
-          </option>
-          {coins.map(({ name, symbol }) => (
-            <option key={`coins-${symbol}`} value={symbol}>
-              {name} ({symbol})
-            </option>
-          ))}
-        </select>
-      )}
+      {coinsLoading && '.'}
+      {coins &&
+        coins.length && (
+          <DivSelect>
+            {controlledValue !== '' && (
+              <Image
+                {...coins.find(({ symbol }) => symbol === controlledValue)}
+              />
+            )}
+            <DivI symbol={controlledValue}>
+              <i className="fas fa-coins" />
+            </DivI>
+            <Select value={controlledValue} onChange={onChangeHandler}>
+              <option disabled value="" hidden>
+                Crypto coins
+              </option>
+              {coins.map(({ name, symbol }) => (
+                <option key={`coins-${symbol}`} value={symbol}>
+                  {name} ({symbol})
+                </option>
+              ))}
+            </Select>
+          </DivSelect>
+        )}
     </Fragment>
   );
 };
