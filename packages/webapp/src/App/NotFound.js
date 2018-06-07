1cd3ba9d274a6caa1b53a2cf3fcb90fb216695e9
Gorka <gorka@aquigorka.com>
2018-06-07 16:58:27 +0200

    404: add designs styles

--- packages/webapp/src/App/NotFound.js
+++ packages/webapp/src/App/NotFound.js
@@ -1,26 +1,30 @@
 import React, { Fragment } from 'react';
 import { NavHeader } from '../components';
 import styled from 'styled-components';
+import { SectionTitle, Section } from '../theme';
 
-const Wrap = styled.div`
+const Big = SectionTitle.extend`
+  font-size: 3em;
+  text-align: center;
+  padding: 1em 0 0;
+  color: #aaa;
+`
+
+const Msg = styled.div`
   margin: 2em;
   text-align: center;
   color: #aaa;
 `;
 
-const H3 = styled.h3`
-  font-size: 30px;
-`;
-
 export default () => (
   <Fragment>
     <NavHeader />
-    <Wrap>
-      <H3>404</H3>
-      <div>
-        Whooooops, we can’t find your money... just joking, the page you
+    <Section>
+      <Big>404</Big>
+      <Msg>
+        Whooooops, we can’t find your money... just kidding, the page you
         requested does not exist.
-      </div>
-    </Wrap>
+      </Msg>
+    </Section>
   </Fragment>
 );
