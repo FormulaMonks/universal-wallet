e32c655adc679c17886b37bd64e4fad2661928bb
Gorka <gorka@aquigorka.com>
2018-06-08 12:54:54 +0200

    Address book: add store, saga and views (book and address)

---
+++ packages/webapp/src/components/AddressBook/Address.js
@@ -0,0 +1,161 @@
+import React, { Component, Fragment } from 'react';
+import styled from 'styled-components';
+import qr from 'qr-encode';
+import {
+  Button,
+  Leaders,
+  LeadersCoins,
+  LeadersQrScan,
+  Dots,
+  H3Address,
+  DivQrPublicAddress,
+} from '../../theme';
+import { Spinner, ImgFromSymbol, CoinsView, Form } from '../';
+
+const LeadersAddress = Leaders.extend`
+  margin-top: 2em;
+`;
+
+const DivBtns = styled.div`
+  display: grid;
+  grid-gap: 1em;
+  margin: 2em auto 0;
+  text-align: center;
+  width: fit-content;
+  grid-template-columns: auto auto;
+
+  & button:nth-of-type(1) {
+    order: 2;
+  }
+`;
+
+const getFormValues = ({ inputAlias, inputPublicAddress, selectSymbol }) => {
+  return {
+    alias: inputAlias.value,
+    publicAddress: inputPublicAddress.value,
+    symbol: selectSymbol.value,
+  };
+};
+
+class AddressView extends Component {
+  state = { publicAddress: '', symbol: '' };
+
+  componentDidMount() {
+    const { address: { publicAddress, symbol } } = this.props;
+
+    if (publicAddress) {
+      this.setState({ publicAddress });
+    }
+
+    if (symbol) {
+      this.setState({ symbol });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { qrData, qrClear } = this.props;
+    if (qrData && !prevProps.qrData) {
+      this.setState({ publicAddress: qrData }, qrClear);
+    }
+    if (prevProps.address.publicAddress !== this.props.address.publicAddress) {
+      this.setState({ publicAddress: this.props.address.publicAddress });
+    }
+  }
+
+  render() {
+    const { coins, coinsLoading, address, qrScan } = this.props;
+    if (coinsLoading) {
+      return <Spinner />;
+    }
+    const { alias } = address;
+    const { publicAddress, symbol } = this.state;
+    const coin = coins.find(c => c.symbol === symbol);
+
+    return (
+      <Fragment>
+        <H3Address>
+          <ImgFromSymbol symbol={symbol} coins={coins} />
+          {alias}
+        </H3Address>
+
+        <DivQrPublicAddress>
+          <img src={qr(publicAddress)} alt={publicAddress} />
+          <div>{publicAddress}</div>
+        </DivQrPublicAddress>
+
+        <Form onSubmit={this.put}>
+          <LeadersAddress>
+            Alias
+            <Dots />
+            <input
+              type="text"
+              name="inputAlias"
+              defaultValue={alias}
+              placeholder="Alias"
+              required
+            />
+          </LeadersAddress>
+
+          <LeadersAddress>
+            Public Address
+            <Dots />
+            <input
+              placeholder="Public Address"
+              type="text"
+              name="inputPublicAddress"
+              value={publicAddress}
+              onChange={this.inputPublicAddressChange}
+              required
+            />
+          </LeadersAddress>
+
+          <LeadersQrScan>
+            <div>Scan from QR Code</div>
+            <Dots />
+            <button type="button" title="Scan from QR Code" onClick={qrScan}>
+              <i className="fas fa-qrcode" />
+            </button>
+          </LeadersQrScan>
+
+          <LeadersCoins>
+            <div>Crypto currency</div>
+            <Dots />
+            <CoinsView
+              required={true}
+              coin={coin}
+              coins={coins}
+              onChange={this.coinChange}
+            />
+          </LeadersCoins>
+
+          <DivBtns>
+            <Button type="submit">Save</Button>
+            <Button type="button" onClick={this.delete}>
+              Delete
+            </Button>
+          </DivBtns>
+        </Form>
+      </Fragment>
+    );
+  }
+
+  coinChange = symbol => {
+    this.setState({ symbol });
+  };
+
+  delete = () => {
+    const { address: { id }, addressBookDelete } = this.props;
+    addressBookDelete(id);
+  };
+
+  inputPublicAddressChange = e => {
+    this.setState({ publicAddress: e.currentTarget.value });
+  };
+
+  put = form => {
+    const { address: { id }, addressBookPut } = this.props;
+    addressBookPut(id, getFormValues(form.elements));
+  };
+}
+
+export default AddressView;
---
+++ packages/webapp/src/components/AddressBook/AddressBook.js
@@ -0,0 +1,175 @@
+import React, { Component, Fragment, Children, cloneElement } from 'react';
+import uuid from 'uuid';
+import { getFile, putFile } from 'blockstack';
+import Compose from '../Compose';
+import AddressBookView from './Book';
+import AddressView from './Address';
+
+const ADDRESS_BOOK_JSON = 'address-book.json';
+
+const sort = (a, b) => a.alias.localeCompare(b.alias);
+
+const View = ({ address, ...rest }) => {
+  if (address) {
+    return <AddressView {...{ address, ...rest }} />;
+  }
+
+  return <AddressBookView {...rest} />;
+};
+
+class Store extends Component {
+  state = {
+    error: null,
+    loading: true,
+    addressBook: [],
+    address: null,
+  };
+
+  render() {
+    const { address, addressBook, loading, error } = this.state;
+    const { children, ...rest } = this.props;
+    return (
+      <Fragment>
+        {Children.map(this.props.children, child =>
+          cloneElement(child, {
+            ...rest,
+            address,
+            addressBook,
+            addressBookError: error && (
+              <div>There was an error fetching the address book: {error}</div>
+            ),
+            addressBookLoading: loading,
+            addressBookGet: this.get,
+            addressBookDelete: this.delete,
+            addressBookPut: this.put,
+            addressBookPost: this.post,
+            addressPick: this.pick,
+            addressRelease: this.release,
+          }),
+        )}
+      </Fragment>
+    );
+  }
+
+  delete = async addressId => {
+    this.setState({ loading: true });
+
+    const newList = this.state.addressBook.filter(({ id }) => id !== addressId);
+    await putFile(ADDRESS_BOOK_JSON, JSON.stringify(newList));
+  };
+
+  get = async () => {
+    this.setState({ loading: true });
+
+    try {
+      const file = await getFile(ADDRESS_BOOK_JSON);
+      const rawAddressBook = JSON.parse(file || '[]');
+      const addressBook = rawAddressBook.sort(sort);
+      this.setState({ addressBook, loading: false });
+    } catch (e) {
+      this.setState({ error: e.toString() });
+    }
+  };
+
+  pick = addressId => {
+    const address = this.state.addressBook.find(({ id }) => addressId === id);
+    if (!address) {
+      return;
+    }
+    this.setState({ address });
+  };
+
+  post = async obj => {
+    this.setState({ loading: true });
+
+    const newObj = {
+      ...obj,
+      id: uuid(),
+      createdAt: Date.now(),
+      lastModified: Date.now(),
+    };
+    const newList = [...this.state.addressBook, newObj];
+    await putFile(ADDRESS_BOOK_JSON, JSON.stringify(newList));
+    return newObj;
+  };
+
+  put = async (addressId, obj) => {
+    this.setState({ loading: true });
+
+    const { addressBook } = this.state;
+    const { id, createdAt, lastModified, ...current } = addressBook.find(
+      ({ id }) => id === addressId,
+    );
+    const newList = [
+      ...addressBook.filter(w => w.id !== id),
+      { ...current, ...obj, id, createdAt, lastModified: Date.now() },
+    ];
+    await putFile(ADDRESS_BOOK_JSON, JSON.stringify(newList));
+  };
+
+  release = () => {
+    this.setState({ address: null });
+  };
+}
+
+class Saga extends Component {
+  componentDidMount() {
+    this.props.addressBookGet();
+  }
+
+  render() {
+    const {
+      children,
+      addressBookDelete,
+      addressBookPut,
+      addressBookPost,
+      ...rest
+    } = this.props;
+
+    return (
+      <Fragment>
+        {Children.map(children, child =>
+          cloneElement(child, {
+            ...rest,
+            addressBookDelete: this.delete,
+            addressBookPut: this.put,
+            addressBookPost: this.post,
+          }),
+        )}
+      </Fragment>
+    );
+  }
+
+  delete = async addressId => {
+    const {
+      address,
+      addressRelease,
+      addressBookDelete,
+      addressBookGet,
+    } = this.props;
+    if (address && address.id === addressId) {
+      addressRelease();
+    }
+    await addressBookDelete(addressId);
+    await addressBookGet();
+  };
+
+  put = async (addressId, data) => {
+    const { addressRelease, addressBookPut, addressBookGet } = this.props;
+    addressRelease();
+    await addressBookPut(addressId, data);
+    await addressBookGet();
+  };
+
+  post = async data => {
+    const { addressBookPost, addressBookGet, addressPick } = this.props;
+    const newAddress = await addressBookPost(data);
+    await addressBookGet();
+    addressPick(newAddress.id);
+  };
+}
+
+const SagaStore = Compose(Store, Saga);
+
+export { SagaStore as Store, View, sort };
+export default Compose(SagaStore, View);
