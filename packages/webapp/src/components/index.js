a61479c426041a7603f0c9a311b8a8614bc7fd0b
Gorka <gorka@aquigorka.com>
2018-06-11 11:08:30 +0200

    Components: add settings to index

--- packages/webapp/src/components/index.js
+++ packages/webapp/src/components/index.js
@@ -25,6 +25,7 @@ export { default as Header } from './Header';
 export { default as ImgFromSymbol } from './ImgFromSymbol';
 export { default as NavHeader } from './NavHeader';
 export { default as NewWallet } from './NewWallet';
+export { default as Settings } from './Settings';
 export { default as Spinner } from './Spinner';
 export { default as QrReader } from './QrReader';
 export {
