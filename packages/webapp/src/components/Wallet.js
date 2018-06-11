87c2128c76986e2d7cc627be35d949eca04f01ad
Gorka <gorka@aquigorka.com>
2018-06-11 11:11:09 +0200

    Wallet: automatically select coin form store when visiting route

--- packages/webapp/src/components/Wallet.js
+++ packages/webapp/src/components/Wallet.js
@@ -1,15 +1,17 @@
 import React, { Component, Fragment, Children, cloneElement } from 'react';
 import qr from 'qr-encode';
-import { Balance, BalanceStore, Currency } from './';
+import { Balance, BalanceStore, Currency, Spinner } from './';
 import Compose from './Compose';
 import { H3Wallet, DivQrPublicAddress, Leaders, Dots } from '../theme';
 
-const View = ({ wallet, walletLoading, coins, coinsLoading }) => {
-  if (!wallet || coinsLoading) {
-    return null;
+const View = ({ wallet, walletsLoading, coins, coinsLoading }) => {
+  if (!wallet || walletsLoading || coinsLoading) {
+    return <Spinner />;
   }
   const { publicAddress, alias, symbol } = wallet;
-  const { imageSmall } = coins.find(c => c.symbol.toLowerCase() === symbol.toLowerCase());
+  const { imageSmall } = coins.find(
+    c => c.symbol.toLowerCase() === symbol.toLowerCase(),
+  );
 
   return (
     <Fragment>
@@ -50,11 +52,23 @@ class Saga extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    const { match: { params: { id } }, wallets, walletsLoading } = this.props;
+    const {
+      match: { params: { id } },
+      wallet,
+      wallets,
+      walletsLoading,
+      coin,
+      coins,
+      coinsLoading,
+    } = this.props;
     if (
       id !== prevProps.match.params.id ||
       walletsLoading !== prevProps.walletsLoading ||
-      wallets.length !== prevProps.wallets.length
+      wallets.length !== prevProps.wallets.length ||
+      (wallet && !prevProps.wallet) ||
+      coinsLoading !== prevProps.coinsLoading ||
+      coins.length !== prevProps.coins.length ||
+      (coin && !prevProps.coin)
     ) {
       this.check();
     }
@@ -79,10 +93,13 @@ class Saga extends Component {
       walletsLoading,
       wallet,
       walletPick,
+      coin,
+      coinPick,
+      coinsLoading,
       match: { params: { id } },
     } = this.props;
 
-    if (walletsLoading) {
+    if (walletsLoading || coinsLoading) {
       return;
     }
     if (!id || !wallets.find(w => w.id === id)) {
@@ -91,7 +108,13 @@ class Saga extends Component {
     }
     if (!wallet) {
       walletPick(id);
+      return;
     }
+    if (!coin) {
+      coinPick(wallet.symbol);
+      return;
+    }
+
     this.setState({ loading: false });
   };
 }
