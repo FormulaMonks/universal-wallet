b52d234de6855a382315f69c62fc9354c65d890f
Gorka <gorka@aquigorka.com>
2018-06-13 10:49:33 +0200

    Add generic abi with getBalanceOf and transfer functions

---
+++ packages/webapp/src/utils/abi.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    constant: true,
+    inputs: [{ name: 'tokenOwner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: 'balance', type: 'uint256' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: 'to', type: 'address' },
+      { name: 'tokens', type: 'uint256' },
+    ],
+    name: 'transfer',
+    outputs: [{ name: 'success', type: 'bool' }],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
