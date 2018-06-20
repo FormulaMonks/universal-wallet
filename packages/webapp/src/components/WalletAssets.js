fcb92d66cd79505bc5a541bb543fe881955c683d
Gorka <gorka@aquigorka.com>
2018-06-20 11:05:29 +0200

    Wallet assets: add view component for wallet assets

---
+++ packages/webapp/src/components/WalletAssets.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { UlGrid, LiGrid, Leaders, Dots } from '../theme';
+import { ImgFromSymbol } from './';
+import styled from 'styled-components';
+
+const UNAVAILABLE = 'Currently unavailable';
+
+const DivLeaders = Leaders.extend`
+  margin-right: 1em;
+`;
+
+const DivAsset = styled.div`
+  margin-top: 2px;
+`;
+
+export default ({ wallet, balances, coins, tokens, totalCurrency }) => {
+  const { id, assets } = wallet;
+
+  return (
+    <UlGrid>
+      {assets.map(symbol => {
+        const { balance } = balances.find(b => b.symbol === symbol) || {
+          balance: UNAVAILABLE,
+        };
+        const { currency } = totalCurrency.find(c => c.symbol === symbol) || {
+          currency: UNAVAILABLE,
+        };
+
+        return (
+          <LiGrid key={`wallet-assets-${symbol}`}>
+            <Link to={`/wallets/${id}/${symbol}`}>
+              <ImgFromSymbol coins={coins} tokens={tokens} symbol={symbol} />
+
+              <DivAsset>
+                <div>{symbol.toUpperCase()}</div>
+                <DivLeaders>
+                  Balance
+                  <Dots />
+                  <div>
+                    {isNaN(balance)
+                      ? balance
+                      : `${symbol.toUpperCase()} ${balance}`}
+                  </div>
+                </DivLeaders>
+
+                <DivLeaders>
+                  USD
+                  <Dots />
+                  <div>{isNaN(currency) ? currency : '$' + currency.toLocaleString()}</div>
+                </DivLeaders>
+              </DivAsset>
+            </Link>
+          </LiGrid>
+        );
+      })}
+    </UlGrid>
+  );
+};
