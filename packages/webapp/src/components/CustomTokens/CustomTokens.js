f8f02d14836a53e42eeab0fb345633acc8dabd7b
Gorka <gorka@aquigorka.com>
2018-06-20 11:27:09 +0200

    Custom tokens: add (temporarily) loaded component

--- packages/webapp/src/components/CustomTokens/CustomTokens.js
+++ packages/webapp/src/components/CustomTokens/CustomTokens.js
@@ -4,6 +4,7 @@ import { getFile, putFile } from 'blockstack';
 import Compose from '../Compose';
 import TokensView from './Tokens';
 import TokenView from './Token';
+import { Spinner } from '../';
 
 const CUSTOM_TOKENS_JSON = 'custom-tokens.json';
 
@@ -170,7 +171,20 @@ class Saga extends Component {
   };
 }
 
+const Loaded = ({ children, ...rest }) => {
+  const { tokensLoading } = rest;
+  if (tokensLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <Fragment>
+      {Children.map(children, child => cloneElement(child, { ...rest }))}
+    </Fragment>
+  );
+};
+
 const SagaStore = Compose(Store, Saga);
 
-export { SagaStore as Store, View, sort };
+export { SagaStore as Store, View, sort, Loaded };
 export default Compose(SagaStore, View);
