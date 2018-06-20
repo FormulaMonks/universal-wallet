0815030461570a439deaaaf5d6de0abbd48e4fa6
Gorka <gorka@aquigorka.com>
2018-06-20 11:21:21 +0200

    Components: update index

--- packages/webapp/src/components/index.js
+++ packages/webapp/src/components/index.js
@@ -1,17 +1,26 @@
 export {
   default as AddressBook,
+  Loaded as AddressBookLoaded,
   Store as AddressBookStore,
   View as AddressBookView,
 } from './AddressBook/AddressBook';
+export { default as Assets } from './Assets';
 export { default as Auth } from './Auth';
 export {
   default as Balance,
   Store as BalanceStore,
   View as BalanceView,
 } from './Balance';
+export {
+  default as Balances,
+  Loaded as BalancesLoaded,
+  Store as BalancesStore,
+  View as BalancesView,
+} from './Balances';
 export { default as BlockstackLink } from './BlockstackLink';
 export {
   default as Coins,
+  Loaded as CoinsLoaded,
   Store as CoinsStore,
   View as CoinsView,
 } from './Coins';
@@ -23,6 +32,7 @@ export {
 } from './Currency';
 export {
   default as CustomTokens,
+  Loaded as CustomTokensLoaded,
   Store as CustomTokensStore,
   View as CustomTokensView,
 } from './CustomTokens/CustomTokens';
@@ -36,15 +46,26 @@ export { default as Settings } from './Settings';
 export { default as Spinner } from './Spinner';
 export { default as QrReader } from './QrReader';
 export {
+  default as TotalCurrency,
+  Loaded as TotalCurrencyLoaded,
+  Store as TotalCurrencyStore,
+  View as TotalCurrencyView,
+} from './TotalCurrency';
+export {
   default as Transactions,
+  Loaded as TransactionsLoaded,
   Store as TransactionsStore,
   View as TransactionsView,
 } from './Transactions';
 export { default as Tx, Store as TxStore, View as TxView } from './Tx';
 export { default as TxSetup } from './TxSetup';
 export { default as WalletView } from './Wallet';
+export { default as WalletAssetPick } from './WalletAssetPick';
+export { default as WalletAssets } from './WalletAssets';
+export { default as WalletPick } from './WalletPick';
 export {
   default as Wallets,
+  Loaded as WalletsLoaded,
   Store as WalletsStore,
   View as WalletsView,
 } from './Wallets';
