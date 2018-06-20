0fc3267300884cd90ed66ac45164f89a56a40e52
Gorka <gorka@aquigorka.com>
2018-06-20 11:06:08 +0200

    Wallet asset pick: add saga component to check and redirect for routes that assume a wallet asset

---
+++ packages/webapp/src/components/WalletAssetPick.js
@@ -0,0 +1,38 @@
+import React, { Component, Fragment, Children, cloneElement } from 'react';
+import { Spinner } from './';
+
+class WalletAssetPick extends Component {
+  state = { loading: true };
+
+  componentDidMount() {
+    this.check();
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <Spinner />;
+    }
+    const { children, ...rest } = this.props;
+
+    return (
+      <Fragment>
+        {Children.map(children, child =>
+          cloneElement(child, { ...rest, ...this.state }),
+        )}
+      </Fragment>
+    );
+  }
+
+  check = () => {
+    const { wallet, match: { params: { symbol } }, history } = this.props;
+
+    const { assets, id } = wallet;
+    if (!assets.includes(symbol)) {
+      history.push(`/wallets/${id}`);
+    }
+
+    this.setState({ loading: false });
+  };
+}
+
+export default WalletAssetPick;
